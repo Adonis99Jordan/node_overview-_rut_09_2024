dd4468535a4071f0118093eddce56cab0fce8dc0
TukuNoni71 <Adonisjzepeda@gmail.com>
2024-09-30 13:10:44 -0400

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+const num1 = +process.argv[2];
+const num2 = parseInt(process.argv[3]);
+
+function sum(a, b) {
+    console.log(a + b);
+}
+
+sum (num1, num2);
+
+
+
+
+// const data = {
+//     name:
+//     info: {
+//         details: {
+//             address:
+//         }
+//     }
+// }
+
+// data.info.details.adrres
