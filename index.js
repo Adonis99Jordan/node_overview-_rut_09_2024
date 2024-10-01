811394d35a6d7c2eb243c7221d47d47c1e95f3b4
TukuNoni71 <Adonisjzepeda@gmail.com>
2024-10-01 10:31:05 -0400

    finished arrow function basics

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-const num1 = +process.argv[2];
-const num2 = parseInt(process.argv[3]);
+const mathtools = {
+    num1: 15,
+    num2: 10,
+    //short hand for sum:function()
+    // sum()
+    sum: function() {
+        //return the sum of num 1 and 2
+        // console.log(module);
+        const nested = () => {
+            console.log('nested', this.num1);
+            const morenested = () => {
+                console.log('morenested', this.num2);
+            }
 
-function sum(a, b) {
-    console.log(a + b);
-}
+            morenested();
+        }
 
-sum (num1, num2);
+        nested();
+
+        return this.num1 + this.num2;
+    },
+    difference: function() {
+        //return the difference of num 1 and 2 (ie. num1- num2)
+        return this.num1 - this.num2;
+    }
+};
+
+const sum = mathtools.sum();
+const diff = mathtools.difference();
+
+console.log('sum', sum);
+console.log('diff', diff);
 
 
 
+// function printName(userName) {
+//     const capitalize = str => str[0].toUpperCase() + str.slice(1);
 
-// const data = {
-//     name:
-//     info: {
-//         details: {
-//             address:
-//         }
-//     }
+//     const capName = capitalize(userName);
+
+//     console.log(capName);
 // }
 
-// data.info.details.adrres
+// printName('sarah');
